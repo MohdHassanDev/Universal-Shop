005d910ce902a97b3813d5731f37e896244ad562
Muhammad Hassan <muhammmadhasssan110786@gmail.com>
2023-03-18 17:58:36 +0500

    deploy project

--- controller/productSchema/imageUpload.js
+++ controller/productSchema/imageUpload.js
@@ -1,19 +1,48 @@
 const cloudinary = require("cloudinary").v2;
-const fs = require("fs");
+const streamifier = require("streamifier");
+
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
 
 const imageUpload = async (req, res) => {
-  const path = req.files[0].path;
-  await cloudinary.uploader.upload(path, (error, data) => {
-    if (error) {
-      res.status(400).json({ message: "error" });
-    } else {
-      fs.unlinkSync(path);
+  await runMiddleware(req, res, uploadMiddleware);
+  console.log(req.file.buffer);
+  const stream = await cloudinary.uploader.upload_stream(
+    {
+      folder: "demo",
+    },
+    (error, data) => {
+      if (error) return console.error(error);
       res.status(200).json({
         message: "success",
         data: { public_id: data.public_id, url: data.secure_url },
       });
     }
-  });
+  );
+  streamifier.createReadStream(req.file.buffer).pipe(stream);
 };
+
+// const imageUpload = async (req, res) => {
+//   const path = req.files[0].path;
+//   await cloudinary.uploader.upload(path, (error, data) => {
+//     if (error) {
+//       res.status(400).json({ message: "error" });
+//     } else {
+//       fs.unlinkSync(path);
+//       res.status(200).json({
+//         message: "success",
+//         data: { public_id: data.public_id, url: data.secure_url },
+//       });
+//     }
+//   });
+// };
 
 module.exports = imageUpload;
--- utils/multer.js
+++ utils/multer.js
@@ -1,11 +1,12 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: "./uploads/",
-  filename: (req, file, cb) => {
-    cb(null, `${new Date().getTime()}-${file.originalname}`);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: "./uploads/",
+//   filename: (req, file, cb) => {
+//     cb(null, `${new Date().getTime()}-${file.originalname}`);
+//   },
+// });
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
